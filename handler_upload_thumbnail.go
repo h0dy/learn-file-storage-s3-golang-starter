868c1b0579cf7500b696cf03326d4f8020b27040
h0dy <hasolanh@gmail.com>
2025-08-04 01:26:59 +0300

    update handlerUploadThumbnail hanlder to parse thumbnail img file and save in assets

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
@@ -23,38 +24,28 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
 		return
 	}
-
 	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	const maxMemory = 10 << 20 // 10 MB
-
 	r.ParseMultipartForm(maxMemory)
-	
 	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Couldn't parse form file", err)
 		return
 	}
 	defer file.Close()
-
 	thumbnailType := header.Header.Get("Content-Type")
 	if thumbnailType == "" {
 		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
 		return
 	}
 
-	fileData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't parse form file", err)
-		return
-	}
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
@@ -65,17 +56,23 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	vidThumbnail := thumbnail{
-		data:fileData,
-		mediaType: thumbnailType,
+	imgPath := getAssetPath(videoID, thumbnailType)
+	imgDiskPath := cfg.getAssetDiskPath(imgPath)
+	dst, err := os.Create(imgDiskPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "something went wrong", err)
+		return
 	}
-	
-	videoThumbnails[videoID] = vidThumbnail
-	thumbURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, videoID.String())
-	video.ThumbnailURL = &thumbURL
-	
+	defer dst.Close()
+	if _, err := io.Copy(dst, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error in saving file", err)
+		return
+	}
+
+	thumbnailURL := cfg.getAssetURL(imgPath)
+	video.ThumbnailURL = &thumbnailURL
+
 	if err := cfg.db.UpdateVideo(video); err != nil {
-		delete(videoThumbnails, videoID)
 		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
 		return
 	}
